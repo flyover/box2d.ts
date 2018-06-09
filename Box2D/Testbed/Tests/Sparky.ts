5ec2915ae85ef6fc405ac91857ca565a7cde59ed
Isaac Burns <isaacburns@gmail.com>
2018-06-09 04:42:51 -0500

    fix es5 error

--- Box2D/Testbed/Tests/Sparky.ts
+++ Box2D/Testbed/Tests/Sparky.ts
@@ -43,19 +43,19 @@ class ParticleVFX {
     //   Math.random(),
     //   Math.random(),
     //   1.0);
+    function hue2rgb(p: number, q: number, t: number) {
+      if (t < 0) { t += 1; }
+      if (t > 1) { t -= 1; }
+      if (t < 1 / 6) { return p + (q - p) * 6 * t; }
+      if (t < 1 / 2) { return q; }
+      if (t < 2 / 3) { return p + (q - p) * (2 / 3 - t) * 6; }
+      return p;
+    }
     function hslToRgb(h: number, s: number, l: number, a: number = 1): box2d.RGBA {
       let r, g, b;
       if (s === 0) {
         r = g = b = l; // achromatic
       } else {
-        function hue2rgb(p: number, q: number, t: number) {
-          if (t < 0) { t += 1; }
-          if (t > 1) { t -= 1; }
-          if (t < 1 / 6) { return p + (q - p) * 6 * t; }
-          if (t < 1 / 2) { return q; }
-          if (t < 2 / 3) { return p + (q - p) * (2 / 3 - t) * 6; }
-          return p;
-        }
         const q: number = l < 0.5 ? l * (1 + s) : l + s - l * s;
         const p: number = 2 * l - q;
         r = hue2rgb(p, q, h + 1 / 3);
